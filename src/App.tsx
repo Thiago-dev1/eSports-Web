66991d0bede7e0bc61caf2cf8b10b4d41b73f7da
Thiago Henrique <tiago@DESKTOP-P5HO0PT.localdomain>
2022-09-16 13:47:32 -0300

    component CreateAdBanner.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { MagnifyingGlassPlus } from 'phosphor-react'
+import { useEffect, useState } from 'react'
+
+import { GameBanner } from './components/GameBanner'
+import { CreateAdBanner } from './components/CreateAdBanner'
 
 import './styles/main.css'
 
 import Logo from "./assets/Logo.svg"
 
-import { GameBanner } from './components/GameBanner'
-import { useEffect, useState } from 'react'
 
 interface Game {
   bannerUrl: string,
@@ -15,7 +16,6 @@ interface Game {
     Ad: number
   }
 }
-
 
 function App() {
 
@@ -40,19 +40,8 @@ function App() {
         })}
       </div>
 
-      <div className='pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
-          </div>
+      <CreateAdBanner />
 
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex gap-3'>
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
     </div>
   )
 }
