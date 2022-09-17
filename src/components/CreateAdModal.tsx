04f62a025a1ac60501ea8960a9159a0d11c78739
Thiago Henrique <tiago@DESKTOP-P5HO0PT.localdomain>
2022-09-17 12:36:59 -0300

    Select

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react"
+
 import * as Dialog from "@radix-ui/react-dialog"
 import * as Checkbox from "@radix-ui/react-checkbox"
 import { Check, GameController } from "phosphor-react"
 
 import { Input } from "./Form/Input"
 
+
+
+interface Game {
+    id: string,
+    title: string,
+  }
+
 export function CreateAdModal() {
+    const [games, setGames] = useState<Game[]>([])
+
+    useEffect(() => {
+        fetch("http://localhost:3333/games")
+          .then(response => response.json())
+          .then(data => setGames(data))
+      }, [])
+
     return (
         <Dialog.Portal>
             <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
@@ -15,7 +32,16 @@ export function CreateAdModal() {
                 <form className='mt-6 flex flex-col gap-4'>
                     <div className='flex flex-col gap-2'>
                         <label htmlFor="game" className='font-semibold'>Qual o game</label>
-                        <Input id="game" placeholder="Selecione o game que deseja jogar" />
+                        <select 
+                            id="game"
+                            className="bg-zinc-900 px-4 py-3 rounded text-sm placeholder:text-zinc-500"
+                        >
+                            <option value="" disabled selected>Selecione o game que deseja jogar</option>
+
+                            {games.map(game => {
+                                return <option key={game.id} value={game.id}>{game.title}</option>
+                            })}
+                        </select>
                     </div>
 
                     <div className='flex flex-col gap-2'>
